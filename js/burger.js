c169a8f8eaed6e453d9e04d105332f4f85c66318
MatweyLyash <matwey.lyashonok@gmail.com>
2024-02-03 14:19:45 +0300

    хз, вроде main готов

--- js/burger.js
+++ js/burger.js
@@ -1,16 +1,29 @@
 const menuIcon = document.querySelector('.burger_bottom');
 const arrowIcon = document.querySelector('.arrow_more');
+const bodyHidden = document.body;
 if (menuIcon) {
     const menuBody = document.querySelector('.burger_list');
     menuIcon.addEventListener('click', () => {
         menuBody.classList.toggle('open');
         menuIcon.classList.toggle('open');
+        bodyHidden.classList.toggle('open');
     });
 }
-if(arrowIcon){
+if (arrowIcon) {
     const addCatalog = document.querySelector('.add_catalog');
-    arrowIcon.addEventListener('click',()=>{
+    arrowIcon.addEventListener('click', () => {
         addCatalog.classList.toggle('open');
         arrowIcon.classList.toggle('open');
     })
 }
+
+const nav = document.querySelector('.header_top');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 50) {
+        nav.style.backgroundColor = '#eee';
+        nav.style.setProperty('--shadow', '0px 4px 4px 0px rgba(0, 0, 0, 0.25)');
+    } else {
+        nav.style.backgroundColor = 'transparent';
+    }
+});
